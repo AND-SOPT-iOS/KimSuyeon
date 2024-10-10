eaef43e27592a8a1997bf5f590d9b5a79b97d48e
mmaybei <yebbi2@ewhain.net>
2024-10-10 21:37:45 +0900

    [init] Initial Commit

---
+++ 35th-Sopt-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  35th-Sopt-Seminar
+//
+//  Created by 예삐 on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
